f231285444587acfc4099685054f092b394c2487
Utif Milkedori <utifmd@gmail.com>
2021-11-25 15:48:29 +0700

    initial commit

---
+++ src/main/kotlin/com/utflnx/who/knows/backend/WhoKnowsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.utflnx.who.knows.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhoKnowsBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhoKnowsBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/utflnx/who/knows/backend/WhoKnowsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.utflnx.who.knows.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhoKnowsBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
